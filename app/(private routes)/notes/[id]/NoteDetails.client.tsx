e69bae8e9d09c8b2d720aff3da9fb28806e0c7d1
BMM <m.bazyk@gmail.com>
2026-02-01 21:27:01 +0200

    fix

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -3,7 +3,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import css from './NoteDetails.module.css';
-import { FetchNotesById } from '@/lib/api/api';
+import { fetchNotesById } from '@/lib/api/clientApi';
 
 function NoteDetailsClient() {
   const { id } = useParams<{ id: string }>();
@@ -14,7 +14,7 @@ function NoteDetailsClient() {
     error,
   } = useQuery({
     queryKey: ['note', id],
-    queryFn: () => FetchNotesById(id),
+    queryFn: () => fetchNotesById(id),
     refetchOnMount: false,
   });
 
--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -5,7 +5,7 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 
-import { FetchNotesById } from '@/lib/api/api';
+import { fetchNotesById } from '@/lib/api/serverApi';
 import NoteDetailsClient from './NoteDetails.client';
 
 interface NoteProps {
@@ -16,7 +16,7 @@ export async function generateMetadata({
   params,
 }: NoteProps): Promise<Metadata> {
   const { id } = await params;
-  const note = await FetchNotesById(id);
+  const note = await fetchNotesById(id);
 
   return {
     title: `${note.title} | NoteHub`,
@@ -43,7 +43,7 @@ async function NoteDetailsPage({ params }: NoteProps) {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
-    queryFn: () => FetchNotesById(id),
+    queryFn: () => fetchNotesById(id),
   });
 
   return (
